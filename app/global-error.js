123591e133b57df943c230eb5ebb9def003c66f3
mask1no <andy.shemy@hotmail.se>
2025-09-17 18:37:13 +0200

    fix(app): stabilize global-error.js to avoid parser errors

--- app/global-error.js
+++ app/global-error.js
@@ -1,4 +1,17 @@
 'use client'
-import * as Sentry from '@sentry / nextjs'
-import Error from 'next / error'
-import, { useEffect } from 'react' export default function G l o balError({ error }) { u s eE ffect(() => { Sentry.c a p tureException(error) }, [error]) r eturn ( < html > < body > < Error /> </ body > </ html > ) }
+import * as Sentry from '@sentry/nextjs'
+import Error from 'next/error'
+import { useEffect } from 'react'
+
+export default function GlobalError({ error }) {
+  useEffect(() => {
+    try { Sentry.captureException(error) } catch {}
+  }, [error])
+  return (
+    <html>
+      <body>
+        <Error statusCode={500} />
+      </body>
+    </html>
+  )
+}
